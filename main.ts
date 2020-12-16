82df19c8801c8818df379e5586f04b70956835c0
Kousuke Ban <bankousuke@gmail.com>
2020-12-16 20:04:32 +0900

    Initial commit

---
+++ main.ts
@@ -0,0 +1,37 @@
+import { Construct } from 'constructs';
+import { App, TerraformStack, TerraformOutput } from 'cdktf';
+import { DynamodbTable } from './.gen/providers/aws/dynamodb-table';
+import { DataAwsRegion, AwsProvider } from './.gen/providers/aws'
+
+export class HelloTerra extends TerraformStack {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    new AwsProvider(this, 'aws', {
+      region: 'eu-central-1'
+    })
+
+    const region = new DataAwsRegion(this, 'region')
+
+    const table = new DynamodbTable(this, 'Hello', {
+      name: `my-first-table-${region.name}`,
+      hashKey: 'temp',
+      attribute: [
+        { name: 'id', type: 'S' },
+      ],
+      billingMode: "PAY_PER_REQUEST"
+    });
+
+    table.addOverride('hash_key', 'id')
+    // table.addOverride('hash_key', 'foo')
+    table.addOverride('lifecycle', { create_before_destroy: true })
+
+    new TerraformOutput(this, 'table_name', {
+      value: table.name
+    })
+  }
+}
+
+const app = new App();
+new HelloTerra(app, 'hello-terra');
+app.synth();
